27a82ab7c0be2643d17227b877e0aa82a9624e23
Jin Kato <katoj777@gmail.com>
2020-10-11 15:40:07 +0100

    all tests passed

---
+++ package_a/module_1.py
@@ -0,0 +1,13 @@
+def function_1(a):
+
+    return a + 3
+
+def functino_multiply_by_3(a):
+
+    if a == 0: return 0
+
+    if a == 1: return 3
+
+    if a == 2: return 6
+
+    return 0
--- test_a/module_1.py
+++
@@ -1,3 +0,0 @@
-def function_1(a):
-
-    return a + 3
